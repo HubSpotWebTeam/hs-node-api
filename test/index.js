1619681beb0ab807d13dca5475ccbcafbeb6e14b
Stephen McElhinney <stephen@appstruct.io>
2017-11-21 21:52:10 +0000

    Fixed a babelrc issue.

--- test/index.js
+++ test/index.js
@@ -2,11 +2,8 @@ import dotenv from 'dotenv';
 import HubSpotAPI from '../src';
 
 dotenv.config();
-
-// Linked to Portal ID 2406023 Developer Portal
-// const hapikey = 'a714cac4-a45c-42ff-9905-da4dc8838d75';
-const { HAPI_KEY } = process.env;
-const hs = new HubSpotAPI({ hapikey: HAPI_KEY });
+const { HAPI_KEY: hapikey } = process.env;
+const hs = new HubSpotAPI({ hapikey });
 
 const blogPosts = hs.blog.getPosts({ name: 'Inbound' });
 
