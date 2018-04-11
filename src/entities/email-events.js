91cce2ff04584252dac5e19424ac0ee02d56df89
Stephen McElhinney <stephen@appstruct.io>
2018-04-11 12:03:13 +0100

    Added JSDoc for email events.

--- src/entities/email-events.js
+++ src/entities/email-events.js
@@ -8,7 +8,11 @@ let _baseOptions;
 
 const getCampaignsWithRecentActivity = async (opts = {}) => {
   try {
-    const mergedProps = Object.assign({}, defaults, _baseOptions, opts);
+    const { offset, limit } = opts;
+    const mergedProps = Object.assign({}, defaults, _baseOptions, {
+      offset,
+      limit
+    });
     const recentCampaigns = await createRequest(
       constants.api.emailEvents.campaignsWithRecentActivity,
       {},
@@ -20,7 +24,7 @@ const getCampaignsWithRecentActivity = async (opts = {}) => {
   }
 };
 
-const getCampaign = async (appId, campaignId) => {
+const getCampaign = async (campaignId, appId) => {
   try {
     const mergedProps = Object.assign({}, defaults, _baseOptions, { appId });
     const campaignInfo = await createRequest(
@@ -38,8 +42,36 @@ const getCampaign = async (appId, campaignId) => {
 
 // const get
 
-export default function workflows(baseOptions) {
+export default function emailEvents(baseOptions) {
   _baseOptions = baseOptions;
 
-  return { getCampaignsWithRecentActivity, getCampaign };
+  return {
+    /**
+     * For a given portal, return all campaign IDs sorted by recent activity associated with the portal.
+     * @async
+     * @memberof hs/emailEvents
+     * @method getCampaignsWithRecentActivity
+     * @param {object} opts
+     * @example
+     * const hs = new HubspotClient(props);
+     * const response = hs.emailEvents.getCampaignsWithRecentActivity(opts);
+     * @property {int} opts.limit
+     * @property {int} opts.offset
+     * @returns {Promise}
+     */
+    getCampaignsWithRecentActivity,
+    /**
+     * For a given campaign, return data associated with the campaign.
+     * @async
+     * @memberof hs/emailEvents
+     * @method getCampaign
+     * @param {int} campaignId Selected campaign id.
+     * @param {int} appId The Application Id for the given email. Found in the get_campaigns endpoint.
+     * @example
+     * const hs = new HubspotClient(props);
+     * const response = hs.emailEvents.getCampaign(campaignId, appId);
+     * @returns {Promise}
+     */
+    getCampaign
+  };
 }
