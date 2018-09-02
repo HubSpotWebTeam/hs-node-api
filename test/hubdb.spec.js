f8972c41e184006a3d87ec86362365d099d80180
Liz Shaw <lizmhs@gmail.com>
2018-09-02 14:28:42 -0400

    Add HubDB spec

---
+++ test/hubdb.spec.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+const HubSpotClient = require('../dist/hubspot-api');
+const expect = require('chai').expect;
+const debug = require('debug')('hubspot-api:tests'); //eslint-disable-line
+const {
+  schemaTable,
+  schemaTables,
+  schemaRows,
+  validate
+} = require('./schemas/hubdb');
+
+const {
+  E2E_TESTS_HUBDB_TABLE_ID: tableId,
+  E2E_TESTS_HUBDB_PORTAL_ID: portalId,
+  E2E_TESTS_HAPI_KEY: hapikey
+} = process.env;
+
+const hs = new HubSpotClient({ hapikey });
+
+describe('Get Tables List', async () => {
+  it('returns a valid list of tables', async () => {
+    const tables = await hs.hubdb.getTables();
+    expect(validate(tables, schemaTables).error).to.be.a('null');
+    return Promise.resolve();
+  });
+});
+
+describe('Create Table', async () => {
+  it('creates a new table and gets a valid response', async () => {
+    const createTableResponse = await hs.hubdb.createTable({
+      name: 'A test table'
+    });
+    expect(validate(createTableResponse, schemaTable).error).to.be.a('null');
+    return Promise.resolve();
+  });
+});
+
+describe('Get table by ID', async () => {
+  it('returns a valid table response', async () => {
+    const getTableResponse = await hs.hubdb.getTableById(tableId, portalId);
+    expect(validate(getTableResponse, schemaTable).error).to.be.a('null');
+    return Promise.resolve();
+  });
+});
+
+describe('Get table rows', async () => {
+  it('returns a valid table rows response', async () => {
+    const getRowsResponse = await hs.hubdb.getTableRows(tableId, portalId);
+    expect(validate(getRowsResponse, schemaRows).error).to.be.a('null');
+    return Promise.resolve();
+  });
+});
