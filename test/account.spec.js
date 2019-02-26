f521b336d3c2e6d66ebf46ad5d26e31dd4442358
Nhori Lopchan Tamang <nlopchantamang@hubspot.com>
2019-02-26 12:27:23 -0500

    removed passing `hapikey`

--- test/account.spec.js
+++ test/account.spec.js
@@ -7,9 +7,9 @@ const { schemaAccount, validate } = require('./schemas/account');
 const { E2E_TESTS_HAPI_KEY: hapikey } = process.env;
 const hs = new HubSpotClient({ hapikey });
 
-describe('Get details for HubSpot account', async (E2E_TESTS_HAPI_KEY) => {
+describe('Get details for HubSpot account', async () => {
   it('returns a valid HubSpot Account details response', async () => {
-    const account = await hs.account.getAccountDetails(E2E_TESTS_HAPI_KEY);
+    const account = await hs.account.getAccountDetails();
     expect(validate(account, schemaAccount).error).to.be.a('null');
   });
 });
