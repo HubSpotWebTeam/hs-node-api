dadb3afc3f621971b218581550a2ef2acce37368
George Kemp <georgekemp94@gmail.com>
2019-03-29 13:57:57 -0400

    BZ-2563 WTAPIOS: Create layouts entity (#25)
    * Adds All Methods to Layouts Entity
    
    * Added Unit Testing for Layout Entity
    
    * Updates Documentation with Layout Entity Methods
    
    * Update README to include env variables for layout id and versionid

--- src/entities/layouts.js
+++ src/entities/layouts.js
@@ -81,6 +81,78 @@ const getLayout = async id => {
   }
 };
 
+const getLayoutBuffer = async id => {
+  try {
+    requiresAuthentication(_baseOptions);
+    if (!id) {
+      throw new Error('getLayoutBuffer requires an `id` argument');
+    }
+    const mergedProps = Object.assign({}, defaults, _baseOptions);
+    const layoutBuffer = await createRequest(
+      constants.api.layouts.getBuffer,
+      { id },
+      mergedProps
+    );
+    return Promise.resolve(layoutBuffer);
+  } catch (e) {
+    return Promise.reject(e.message);
+  }
+};
+
+const hasBufferedChanges = async id => {
+  try {
+    requiresAuthentication(_baseOptions);
+    if (!id) {
+      throw new Error('hasBufferedChanges requires an `id` argument');
+    }
+    const mergedProps = Object.assign({}, defaults, _baseOptions);
+    const result = await createRequest(
+      constants.api.layouts.hasBufferedChanges,
+      { id },
+      mergedProps
+    );
+    return Promise.resolve(result);
+  } catch (e) {
+    return Promise.reject(e.message);
+  }
+};
+
+const getPreviousLayoutVersions = async id => {
+  try {
+    requiresAuthentication(_baseOptions);
+    if (!id) {
+      throw new Error('getPreviousLayoutVersions requires an `id` argument');
+    }
+    const mergedProps = Object.assign({}, defaults, _baseOptions);
+    const previousVersions = await createRequest(
+      constants.api.layouts.getPreviousVersions,
+      { id },
+      mergedProps
+    );
+    return Promise.resolve(previousVersions);
+  } catch (e) {
+    return Promise.reject(e.message);
+  }
+};
+
+const getPreviousLayoutVersion = async ({ id, versionId }) => {
+  try {
+    requiresAuthentication(_baseOptions);
+    if (!id || !versionId) {
+      throw new Error('getPreviousLayoutVersion requires the first argument to contain both keys for `id` & `versionId`');
+    }
+    const mergedProps = Object.assign({}, defaults, _baseOptions);
+    const previousVersions = await createRequest(
+      constants.api.layouts.getPreviousVersions,
+      { id, versionId },
+      mergedProps
+    );
+    return Promise.resolve(previousVersions);
+  } catch (e) {
+    return Promise.reject(e.message);
+  }
+};
+
 export default function layoutsApi(baseOptions) {
   _baseOptions = baseOptions;
 
@@ -120,6 +192,56 @@ export default function layoutsApi(baseOptions) {
      * hs.layouts.getLayout(layoutId).then(response => console.log(response));
      * @returns {Promise}
      */
-    getLayout
+    getLayout,
+    /**
+     * Gets the current contents of the auto-save buffer
+     * @async
+     * @memberof hs/layouts
+     * @method getLayoutBuffer
+     * @param {int} layoutId
+     * @example
+     * const hs = new HubspotClient(props);
+     * hs.layout.getLayoutBuffer(layoutId).then(response => console.log(response));
+     * @return {Promise}
+     */
+    getLayoutBuffer,
+    /**
+     * Returns a dictionary: {"has_changes": false/true} depending on if the buffer is different from the live object.
+     * @async
+     * @memberof hs/layouts
+     * @method hasBufferedChanges
+     * @param {int} layoutId
+     * @example
+     * const hs = new HubspotClient(props);
+     * hs.layout.hasBufferedChanges(layoutId).then(response => console.log(response));
+     * @return {Promise}
+     */
+    hasBufferedChanges,
+    /**
+     * Get the previous revisions for a specific layout, specified by ID.
+     * @async
+     * @memberof hs/layouts
+     * @method getPreviousLayoutVersions
+     * @param {int} layoutId
+     * @example
+     * const hs = new HubspotClient(props);
+     * hs.layout.getPreviousLayoutVersions(layoutId).then(response => console.log(response));
+     * @return {Promise}
+     */
+    getPreviousLayoutVersions,
+    /**
+     * Get a specific revision of a specific layout. Version id is the id of the version from the list previous versions endpoint
+     * @async
+     * @memberof hs/layouts
+     * @method getPreviousLayoutVersion
+     * @param {object} opts
+     * @param {int} opts.id - layoutId
+     * @param {int} opts.versionId - id of the versionm from the list previous versions endpoint
+     * @example
+     * const hs = new HubspotClient(props);
+     * hs.layout.getPreviousLayoutVersion({ id , versionId }).then(response => console.log(response));
+     * @return {Promise}
+     */
+    getPreviousLayoutVersion,
   };
 }
