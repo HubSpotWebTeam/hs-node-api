d54f178438fda06f5140b93558ce73c5c79d62ad
Stephen McElhinney <smcelhinney@hubspot.com>
2019-04-26 11:17:06 +0100

    Moved scripts to travis folder, added check for PR branch

---
+++ travis/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "Building components"
+
+# if [[ "$TRAVIS_BRANCH" == "release" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+#   echo "Creating docs for release branch"
+#   if [ -n "$GITHUB_API_TOKEN" ]; then
+#     cd "$TRAVIS_BUILD_DIR"
+#     # This generates a `web` directory containing the website.
+#     echo "Starting docs compilation"
+#     node ./node_modules/.bin/jsdoc -R README.md -c ./jsconf.json -t ./node_modules/ink-docstrap/template -r ./src -d ./
+#     git init
+#     git checkout -b gh-pages
+#     git add .
+#     git -c user.name='smcelhinney' -c user.email='stephen@appstruct.io' commit -m init
+#     # Make sure to make the output quiet, or else the API token will leak!
+#     # This works because the API key can replace your password.
+#     git push -f -q https://smcelhinney:$GITHUB_API_TOKEN@github.com/HubSpotWebTeam/hs-node-api gh-pages:gh-pages &>/dev/null
+#     cd "$TRAVIS_BUILD_DIR"
+#   fi
+# fi
