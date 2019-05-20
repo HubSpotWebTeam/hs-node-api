80dff49ad85fccd7c8257143ef87af72d9047ca2
Stephen McElhinney <smcelhinney@hubspot.com>
2019-05-20 10:51:46 +0100

    Updated travis.yml to only test on master

--- travis/postbuild.sh
+++ travis/postbuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Running post build script"
 
-if [ "$TRAVIS_BRANCH" = "master" ]; then
-  echo "Running integration tests on master"
+if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" = "master" ]]; then
+  echo "Running integration tests on master (non pull request)"
   npm test
 fi
