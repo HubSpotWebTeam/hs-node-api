4f53243144ddf1a4c07e16fbaf6081befe3e46fc
George Kemp <georgekemp94@gmail.com>
2019-06-11 15:17:47 +0100

    Adds Form Submission V3 (#45)

--- src/entities/forms.js
+++ src/entities/forms.js
@@ -5,6 +5,26 @@ import constants from '../constants';
 const defaults = {};
 let _baseOptions;
 
+const submitFormV3 = async (portalId, formId, opts = {}) => {
+  try {
+    const method = 'POST';
+    const response = await createRequest(
+      constants.api.forms.submitFormV3,
+      {
+        formId,
+        portalId,
+        method,
+        body: {
+          ...opts,
+        }
+      }
+    );
+    return Promise.resolve(response);
+  } catch (e) {
+    return Promise.reject(e);
+  }
+}
+
 const submitForm = async (portalId, formId, opts = {}) => {
   try {
     // hs-context params
@@ -119,6 +139,20 @@ export default function formsApi(baseOptions) {
      * const hs = new HubSpotClient(props);
      * const submissions = await hs.forms.getSubmissions(formId)
      */
-    getSubmissions
+    getSubmissions,
+    /**
+     * Submit a form with data See the {@link https://developers.hubspot.com/docs/methods/forms/submit_form_v3|developer docs} for full spec.
+     * @async
+     * @memberof hs/forms
+     * @method submitFormV3
+     * @param {int} portalId Portal ID the form resides on
+     * @param {string} formId ID of form to submit.
+     * @param {object} submitBody { fields, context, legalConsentOptions } see docs for full spec
+     * @example
+     * const hs = new HubspotClient(props);
+     * hs.forms.submitFormV3(portalId, formId, submitBody).then(response => console.log(response));
+     * @returns {Promise}
+     */
+    submitFormV3,
   };
 }
