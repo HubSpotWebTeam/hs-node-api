67646e9b6aeb6ae3be6672ffc62237229e3ffadf
Taylor Swyter <Tswyter@gmail.com>
2019-10-16 10:05:00 -0400

    adds template updating (#54)
    * adds template updating
    
    * removes hard-coded headers from createRequest

---
+++ test/templates.spec.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const HubSpotClient = require('../dist/hubspot-api');
+const expect = require('chai').expect;
+
+const {
+  E2E_TESTS_HAPI_KEY: hapikey,
+  E2E_TESTS_TEMPLATE_ID: templateId,
+} = process.env;
+
+const hs = new HubSpotClient({ hapikey });
+
+describe('Update Template', async () => {
+  it('updates a template in a portal', async () => {
+    const updateValue = '<p>Test</p>';
+    const template = await hs.templates.updateTemplate(templateId, JSON.stringify({ source: updateValue }));
+    expect(template.id.toString()).to.equal(templateId);
+    return Promise.resolve();
+  });
+});
