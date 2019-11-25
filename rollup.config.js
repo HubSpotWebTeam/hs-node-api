937d713469cc646d45c8cfb74a8a7abfe55d4fa1
Alessio Michelini <darkmavis1980@users.noreply.github.com>
2019-11-25 14:36:06 +0000

    Feature/add get templates endpoint (#69)
    * Fixed typo in the JSDoc for the templates
    Updated jsdoc to latest version
    
    * Added get templates endpoint
    
    * Fixed rollup deprecation notice
    
    * Removed browser only global flags for eslint
    
    * Updated outdated dependencies
    
    * Removed unncessary eslint globals
    
    * Added .env.sample file
    Fixed issue with failing tests
    
    * Fixed some commented out codes
    Simplified tests for the hubdb methods
    
    * Added support for updating the template with the updateAutosaveBuffer method

--- rollup.config.js
+++ rollup.config.js
@@ -4,8 +4,9 @@ import commonjs from 'rollup-plugin-commonjs';
 import json from 'rollup-plugin-json';
 import builtins from 'rollup-plugin-node-builtins';
 import globals from 'rollup-plugin-node-globals';
-import { version, author } from './package.json';
 import { uglify } from 'rollup-plugin-uglify';
+import { version, author } from './package.json';
+
 
 const banner =
   `${'/*!\n * Hubspot Client API v'}${version}\n` +
@@ -38,8 +39,10 @@ export default {
       runtimeHelpers: true
     }),
     resolve({
-      jsnext: true,
-      main: true,
+      mainFields: [
+        'jsnext',
+        'main'
+      ],
       browser: false,
       preferBuiltins: true
     }),
