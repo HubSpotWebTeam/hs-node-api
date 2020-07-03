3b5c970e691a162e831bcdcd196938766d69b4e0
Matt <matthewstaff@gmail.com>
2020-07-03 06:38:53 -0400

    Feature/files test spec joi schema (#94)
    * Feature: Add Files Entity Test Spec
    - add test/files.spec.js
    - add test/schemas/file.js
    
    * bump package.json version
    
    * - Update contributing docs to add process info
    - Revert package.json version bump
    
    * bump package.json to match upstream version
    
    Co-authored-by: Matt Staff <mstaff@hubspot.com>

---
+++ test/files.spec.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const { expect } = require('chai');
+const debug = require('debug')('hubspot-api:tests'); //eslint-disable-line
+const HubSpotClient = require('../dist/bundle.min');
+const { schemaFiles, validate } = require('./schemas/file');
+
+const { E2E_TESTS_FOLDER_ID: folderId, E2E_TESTS_HAPI_KEY: hapikey } = process.env;
+
+const hs = new HubSpotClient({ hapikey });
+
+describe('Entity:files Get files in Folder', async () => {
+  it('gets file given folderId', async () => {
+    const response = await hs.files.getFilesInFolder(folderId);
+    const valid = validate(response, schemaFiles);
+    expect(valid.error).to.be.a('null');
+    return Promise.resolve();
+  });
+});
+
+describe('Entity:files Get folders in parent folder with 2 subdirectories', async () => {
+  it('gets sub folders', async () => {
+    const { total_count } = await hs.files.getFolders(folderId);
+    expect(total_count).to.equal(2);
+  });
+});
